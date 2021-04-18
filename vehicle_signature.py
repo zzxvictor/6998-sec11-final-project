7f31b16e0fbbc351378d4e2e557d3dbcd18a2e03
Haoran Zhu <hz3fr@virginia.edu>
2021-04-17 21:13:55 -0400

    update siamese network

--- vehicle_signature.py
+++ vehicle_signature.py
@@ -1,20 +1,3 @@
-<<<<<<< Updated upstream
-from utils.data_loader import  DataLoader4Signature
-from configs import constants
-import matplotlib.pyplot as plt
-
-
-if __name__ == '__main__':
-    data_loader = DataLoader4Signature(constants.DATA_ROOT,
-                                       constants.ANNOTATION_FILES)
-    train, val = data_loader.load(batch_size=32, repeat=True)
-    for idx, (img1, img2, labels) in enumerate(train.take(5)):
-        fig, axs = plt.subplots(1, 2)
-        axs[0].imshow(img1[1].numpy() + 0.5)
-        axs[1].imshow(img2[1].numpy() + 0.5)
-        plt.title(str(idx) + ':'+ str(labels[1]))
-        plt.show()
-=======
 from utils.data_loader import DataLoader4Signature
 import numpy as np
 import pandas as pd
@@ -73,4 +56,3 @@ print("Device:", device)
 model = train()
 torch.save(model.state_dict(), "model.pt")
 print("Model Saved Successfully")
->>>>>>> Stashed changes
